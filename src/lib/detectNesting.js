8ac83866bce35aa88e42915702f6a03dda2a6cc7
Robin Malfait <malfait.robin@gmail.com>
2021-11-08 16:51:04 +0100

    improve nesting detection

--- src/lib/detectNesting.js
+++ src/lib/detectNesting.js
@@ -2,6 +2,20 @@ export default function (_context) {
   return (root, result) => {
     let found = false
 
+    root.walkAtRules('tailwind', (node) => {
+      if (found) return false
+
+      if (node.parent && node.parent.type !== 'root') {
+        found = true
+        node.warn(
+          result,
+          // TODO: Improve this warning message
+          'Nested @tailwind rules detected, instead try to use a prefix: https://tailwindcss.com/docs/configuration#prefix'
+        )
+        return false
+      }
+    })
+
     root.walkRules((rule) => {
       if (found) return false
 
@@ -12,7 +26,6 @@ export default function (_context) {
           // TODO: Improve this warning message
           'Nested CSS detected, checkout the docs on how to support nesting: https://tailwindcss.com/docs/using-with-preprocessors#nesting'
         )
-
         return false
       })
     })
